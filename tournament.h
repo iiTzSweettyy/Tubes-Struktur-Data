8e32f338a637f9fa2716a1206bfee8b76b91cc40
Putra Anugrah Pamungkas <panugrahpamungkas@gmail.com>
2025-11-30 20:56:19 +0700

    Update code tournament.h

--- tournament.h
+++ tournament.h
@@ -2,6 +2,7 @@
 #define TOURNAMENT_H
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +10,58 @@ using namespace std;
 struct MatchNode {
     string teamName;      
     string matchLabel;    
-    MatchNode* left;      
-    MatchNode* right;     
+    int matchID;          
+    MatchNode* left;
+    MatchNode* right;
 
-    MatchNode(string name, string label);
+    MatchNode(string name, string label, int id);
+};
+
+// CLASS TOURNAMENT
+class Tournament {
+private:
+    MatchNode* root;
+    int matchCounter; 
+
+    // Internal Helpers
+    MatchNode* buildTreeRecursive(vector<string>& currentTeams);
+    void printRecursive(MatchNode* node, int space);
+    void destroyTree(MatchNode* node);
+    void processAutoWinByes(MatchNode* node); 
+    void getOpenMatches(MatchNode* node, vector<MatchNode*>& list);
+    bool searchRecursive(MatchNode* node, string targetName);
+    
+    // Traversals
+    void preOrderRec(MatchNode* node);
+    void inOrderRec(MatchNode* node);
+    void postOrderRec(MatchNode* node);
+
+public:
+    Tournament();
+    ~Tournament();
+
+    // Core Functions
+    void initBracket(vector<string> teams); 
+    void showBracket();                            
+    
+    // Admin & Logic
+    void showActiveMatches();                      
+    void updateMatchWinner(int id, string winner); 
+    
+    // General Features
+    void findTeamStatus(string teamName);
+    void printAllTraversals();
+    bool isReady();
+};
+
+// CLASS ADMIN AUTH
+class AdminAuth {
+private:
+    string username;
+    string password;
+public:
+    AdminAuth();
+    bool login();
 };
 
 #endif
