31d4357aaad801c4fb336d30f3df28437cc3aeed
Ulung Putra S. <akhmad.b1103@gmail.com>
2025-12-08 16:17:10 +0700

    Update File header

--- tournament.h
+++ tournament.h
@@ -6,52 +6,50 @@
 
 using namespace std;
 
-// STRUKTUR NODE
 struct MatchNode {
     string teamName;      
     string matchLabel;    
-    int matchID;          
+    int matchID;
     MatchNode* left;
     MatchNode* right;
 
     MatchNode(string name, string label, int id);
 };
 
-// CLASS TOURNAMENT
 class Tournament {
 private:
     MatchNode* root;
-    int jumlahMatch; 
-
+    int matchCounter; 
 
     MatchNode* buildTree(vector<string>& currentTeams);
+    void printTree(MatchNode* node, int space);    
     void printRecursive(MatchNode* node, int space);
     void destroyTree(MatchNode* node);
     void byesAutoWin(MatchNode* node); 
     void nungguMatch(MatchNode* node, vector<MatchNode*>& list);
     bool searching(MatchNode* node, string targetName);
-    
-    // Traversals
+
     void preOrder(MatchNode* node);
     void inOrder(MatchNode* node);
     void postOrder(MatchNode* node);
 
+public:
+    Tournament();
+    ~Tournament();
 
-    // Function Bracket
     void bikinBracket(vector<string> teams); 
     void showBracket();                            
     
-    // Function Admin
+    // Fitur Admin  
     void showActiveMatches();                      
     void updateMatchWinner(int id, string winner); 
     
-    
+    // Fitur Umum
     void findTeamStatus(string teamName);
     void printAllTraversals();
     bool cekBracket();
 };
 
-// CLASS ADMIN AUTH
 class AdminAuth {
 private:
     string username;
